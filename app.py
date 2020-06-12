ec33e97b094978b24db0d9e351f96a7d061da08c
ldoney <lincolndoney@outlook.com>
2020-06-12 22:33:09 +0100

    Wrapping with flask

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request
+import json
+import time
+import RPi.GPIO as io # using RPi.GPIO
+
+left_pin = 10
+right_pin = 8
+STOP = 0
+
+def setup_GPIO():
+	print("Setting up GPIO...")
+	io.setmode(io.BOARD)
+	io.setup(left_pin,io.OUT, initial=io.LOW)
+	io.setup(right_pin,io.OUT, initial=io.LOW)
+	print("Done setting up!")
+
+def move(sec, pin, value):
+	print("Moving " + str(pin) + " for " + str(sec) + " seconds")
+	drive_time(sec, pin, value)
+	drive_motor(pin, STOP)
+	print("Done moving!")
+
+def drive_time(sec, pin, value):
+	io.output(pin, value)
+	time.sleep(sec)
+
+def drive_motor(pin, value):
+	io.output(pin,value)
+
+def run_task(str):
+	exec(str)
+
+def forward(sec, power): 
+	print("Driving motors with " + str(power) + " power for " + str(sec) + " seconds!")
+	drive_motor(left_pin, power) 
+	drive_motor(right_pin, power)
+	time.sleep(sec)
+	print("Stopping...")
+	drive_motor(left_pin, STOP)
+	drive_motor(right_pin, STOP)
+
+
+setup_GPIO()
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/robot.py', methods=["GET","POST"])
+def robot():
+	print(request.form['run'])
+	run_task(request.form['run'])
+	return "Sent"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
---
+++ static/js/google-blockly/custom_block.js
@@ -0,0 +1,42 @@
+Blockly.defineBlocksWithJsonArray([{
+  "type": "go_forward",
+  "message0": "Go Forward for _ seconds: %1 Power: %2",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "seconds",
+      "check": "Number"
+    },
+    {
+      "type": "input_value",
+      "name": "power",
+      "check": "Number"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 230,
+  "tooltip": "",
+  "helpUrl": ""
+},
+{
+  "type": "go_backward",
+  "message0": "Go Backwards for _ seconds: %1 Power: %2",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "seconds",
+      "check": "Number"
+    },
+    {
+      "type": "input_value",
+      "name": "power",
+      "check": "Number"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 60,
+  "tooltip": "",
+  "helpUrl": ""
+}])
