74dff7954ebf47ab38638475a7d7fe9ed421fb9a
YashDRaja <56655681+YashDRaja@users.noreply.github.com>
2020-06-12 16:32:06 -0400

    s

--- google-blockly/custom_block.js
+++ google-blockly/custom_block.js
@@ -18,4 +18,25 @@ Blockly.defineBlocksWithJsonArray([{
   "colour": 230,
   "tooltip": "",
   "helpUrl": ""
+},
+{
+  "type": "go_backward",
+  "message0": "Go Backwards for _ seconds: %1 Power: %2",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "seconds",
+      "check": "Number"
+    },
+    {
+      "type": "input_value",
+      "name": "power",
+      "check": "Number"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 60,
+  "tooltip": "",
+  "helpUrl": ""
 }])
--- google-blockly/generators/python/custom_generator.js
+++ google-blockly/generators/python/custom_generator.js
@@ -5,4 +5,11 @@ Blockly.Python['go_forward'] = function(block) {
   // TODO: Assemble Python into code variable.
   var code = 'forward('+value_seconds +","+value_power+")\n";
   return code;
+};
+Blockly.Python['go_backward'] = function(block) {
+  var value_seconds = Blockly.Python.valueToCode(block, 'seconds', Blockly.Python.ORDER_ATOMIC);
+  var value_power = Blockly.Python.valueToCode(block, 'power', Blockly.Python.ORDER_ATOMIC);
+  // TODO: Assemble Python into code variable.
+  var code = 'backward('+value_seconds +","+value_power+")\n";
+  return code;
 };	
