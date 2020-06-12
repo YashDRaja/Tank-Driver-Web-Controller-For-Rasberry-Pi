ec7e9fdf786f28e5202a6858938871e61eee02a0
YashDRaja <56655681+YashDRaja@users.noreply.github.com>
2020-06-12 13:25:34 -0400

    d

---
+++ google-blockly/go_forward_block.js
@@ -0,0 +1,16 @@
+Blockly.defineBlocksWithJsonArray([{
+  "type": "go_forward",
+  "message0": "Go Forward for _ seconds: %1",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "seconds",
+      "check": "Number"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 230,
+  "tooltip": "",
+  "helpUrl": ""
+}])
---
+++ google-blockly/go_forward_generator.js
@@ -0,0 +1,6 @@
+Blockly.Python['go_forward'] = function(block) {
+  var value_seconds = Blockly.Python.valueToCode(block, 'seconds', Blockly.Python.ORDER_ATOMIC);
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
